b903ff606f0a663a7cbe8382eacba998a2df9ad3
Ardumine <david.codeco@gmail.com>
2022-08-07 11:39:00 +0100

    as

--- installer.py
+++ installer.py
@@ -14,7 +14,7 @@ create_dir("~/nvdli-data/machine")
 files = os.listdir("src")
 for file in files:
     print(file)
-    os.system("src" + file, "~/nvdli-data/machine")
+    os.system("cp src/" + file, "~/nvdli-data/machine")
 
 open("~/run_machine.sh", "w").write("sudo docker run --expose 8080  --runtime nvidia -it --rm --network host     --volume ~/nvdli-data:/nvdli-nano/data     --device /dev/video0     nvcr.io/nvidia/dli/dli-nano-ai:v2.0.1-r32.6.1 cd /nvdli-nano/data/machine/ && python3 main.py")
 
