4f5646e1241635892440f4c456dbf411ad4f58e3
Ardumine <david.codeco@gmail.com>
2022-08-07 11:34:13 +0100

    as

--- installer.py
+++ installer.py
@@ -3,17 +3,17 @@ import shutil
 print("Please wait...")
 def create_dir (dir):
     try:
-        os.mkdir(dir)
+        os.system("mkdir" + dir)
     except:
         pass
 
 create_dir("~/nvdli-data")
 create_dir("~/nvdli-data/classification")
-create_dir("~/nvdli-data/classification/machine")
+create_dir("~/nvdli-data/machine")
 
 files = os.listdir("src")
 for file in files:
-    shutil.copy(file, "~/nvdli-data/classification/machine")
+    shutil.copy(file, "~/nvdli-data/machine")
 
 open("~/run_machine.sh", "w").write("sudo docker run --expose 8080  --runtime nvidia -it --rm --network host     --volume ~/nvdli-data:/nvdli-nano/data     --device /dev/video0     nvcr.io/nvidia/dli/dli-nano-ai:v2.0.1-r32.6.1 cd /nvdli-nano/data/machine/ && python3 main.py")
 
