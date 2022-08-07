aec6c56ac1e0f38954134849155ae0cf12c62164
Ardumine <david.codeco@gmail.com>
2022-08-07 11:21:03 +0100

    as

--- installer.py
+++ installer.py
@@ -3,4 +3,4 @@ print("Please wait...")
 os.mkdir("~/nvdli-data")
 os.mkdir("~/nvdli-data/classification")
 
-open("~/run_machine.sh", "w").write("sudo docker run --expose 8080  --runtime nvidia -it --rm --network host     --volume ~/nvdli-data:/nvdli-nano/data     --device /dev/video0     nvcr.io/nvidia/dli/dli-nano-ai:v2.0.1-r32.6.1 python3 /nvdli-nano/data/trabalho/Main.py")
+open("~/run_machine.sh", "w").write("sudo docker run --expose 8080  --runtime nvidia -it --rm --network host     --volume ~/nvdli-data:/nvdli-nano/data     --device /dev/video0     nvcr.io/nvidia/dli/dli-nano-ai:v2.0.1-r32.6.1 python3 /nvdli-nano/data/trabalho/main.py")
